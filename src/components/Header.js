5f79084f82b44b6716e9432937dec76b4627f70b
Johnson <23009886@myrp.edu.sg>
2025-01-15 02:50:10 +0800

    title change

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,8 +10,8 @@ export default function Header() {
         <img
           className="logo"
           src={logo}
-          alt="Red30 Tech logo"
-          title="Red30 Tech | Home"
+          alt="RP logo"
+          title="RP | Home"
         />
       </Link>
 
